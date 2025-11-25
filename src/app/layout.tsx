70614ffb3980bd7f9645730eca8f078bc7576ab3
Enock Banda <enockbanda300@gmail.com>
2025-11-25 04:51:50 +0000

    still manage department is returning same error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,10 +13,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-  auth,
 }: Readonly<{
   children: React.ReactNode;
-  auth: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
@@ -40,7 +38,6 @@ export default function RootLayout({
         <FirebaseClientProvider>
           <AppShell>{children}</AppShell>
         </FirebaseClientProvider>
-        {auth}
         <Toaster />
       </body>
     </html>
