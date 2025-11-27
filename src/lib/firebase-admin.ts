48152f429445eb5bd40d6d14c3da36382153badf
Enock Banda <enockbanda300@gmail.com>
2025-11-27 09:22:49 +0000

    Try fixing this error: `Runtime Error: An unexpected response was receiv

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -2,10 +2,17 @@ import admin from 'firebase-admin';
 import { firebaseConfig } from '@/firebase/config';
 
 if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
-    databaseURL: firebaseConfig.databaseURL,
-  });
+  try {
+    admin.initializeApp({
+      credential: admin.credential.applicationDefault(),
+      databaseURL: firebaseConfig.databaseURL,
+    });
+  } catch (error: any) {
+    if (!/already exists/u.test(error.message)) {
+      console.error('Firebase admin initialization error', error.stack);
+    }
+  }
 }
 
+export default admin;
 export const db = admin.database();
