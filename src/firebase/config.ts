32d9027a17d8671023450f59101d5771f64ad72f
Enock2000 <enock2000@example.com>
2025-12-01 02:25:24 +0200

    Update: Use official Firebase config from console (includes storage bucket)

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,10 @@
 export const firebaseConfig = {
-  "projectId": "studio-9903628032-db490",
-  "appId": "1:414769317546:web:5355c7bccea67c1e6172d3",
   "apiKey": "AIzaSyDFOr0AMAkxZ_oMf2aAwOi2TskISfy-UGg",
   "authDomain": "studio-9903628032-db490.firebaseapp.com",
-  "measurementId": "",
+  "databaseURL": "https://studio-9903628032-db490-default-rtdb.firebaseio.com",
+  "projectId": "studio-9903628032-db490",
+  "storageBucket": "studio-9903628032-db490.firebasestorage.app",
   "messagingSenderId": "414769317546",
-  "databaseURL": "https://studio-9903628032-db490-default-rtdb.firebaseio.com"
+  "appId": "1:414769317546:web:5355c7bccea67c1e6172d3"
 };
+
